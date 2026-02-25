8ffe6f86cd3c9b8e65772380e51a4cca592f3dda
Gabriel <gabrielplautz56@gmail.com>
2026-02-25 17:27:08 -0300

    Creating main loop Connecting Menu and Game classes

--- code/game.py
+++ code/game.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
+from code.Menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.DistanciaAtual = None
-        self.Record_distancia = None
-        self.estado_jogo = None
-        self.lista_obstaculos = None
-        self.arquivo_save = "recorde.txt"
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 400))
 
-    def iniciar(self, ):
-        pass
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+            # Check for all events
+            #for event in pygame.event.get():
+             #   if event.type == pygame.QUIT:
+              #      pygame.quit()  # Close Window
+               #     quit()  # end pygame
 
-    def loop_principal(self, ):
-        pass
-
-    def verificar_colisoes(self, ):
-        pass
-
-    def carregar_recorde(self, ):
-        pass
-
-    def salvar_recorde(self, ):
-        pass
-
-    def atualizar_hud(self, tela):
-        pass
